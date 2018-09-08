51db0af0f091d8c604aa4d29d0bf53fdaef1cc72
wbp <1871960139@qq.com>
2018-09-08 17:27:40 +0800

    两种方式整合Servlet

--- spring-boot-servlet/src/main/java/com/wbp/example/App.java
+++ spring-boot-servlet/src/main/java/com/wbp/example/App.java
@@ -1,11 +1,8 @@
-package com.example;
+package com.wbp.example;
 
-import com.example.servlet.MyServlet1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 
 /**
@@ -15,6 +12,16 @@ import org.springframework.context.annotation.Bean;
 @ServletComponentScan //启动servlet扫描
 public class App {
 
+    /**
+     * 第一种方式：
+     *      1.创建Servlet（MyServlet1）
+     *      2.通过ServletRegistrationBean注册Servlet
+     *
+     * 第二种方式：
+     *      1.启动类上添加注解@ServletComponentScan
+     *      2.创建Servlet（MyServlet2），并添加类注解@WebServlet
+     *
+     */
     /*@Bean
     public ServletRegistrationBean myServlet(){
         return new ServletRegistrationBean(new MyServlet1(),"/myServlet1");
--- spring-boot-servlet/src/main/java/com/wbp/example/servlet/MyServlet1.java
+++ spring-boot-servlet/src/main/java/com/wbp/example/servlet/MyServlet1.java
@@ -1,4 +1,4 @@
-package com.example.servlet;
+package com.wbp.example.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
